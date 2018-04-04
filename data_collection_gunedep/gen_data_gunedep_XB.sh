5c91ef89f011629dee2ce18fad7f66b3bd29d4ac
Jacob Olander <31401560+mrjacola@users.noreply.github.com>
2018-04-04 20:29:46 +0200

    Update gen_data_gunedep_XB.sh

--- data_collection_gunedep/gen_data_gunedep_XB.sh
+++ data_collection_gunedep/gen_data_gunedep_XB.sh
@@ -2,6 +2,8 @@
 
 # $1 is root file from simulation with XB
 
+# This code is for XB only
+
 root -q 'root_make_class_and_gen_data_files_XB.C('\"$1\"')'
 echo "ROOT finished"
 echo "Removing h102.C and h102.h"
